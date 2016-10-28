694fdf94db98c04c68e58111bd4a6bce9b7c9af8
akoski <akoski@kth.se>
2016-10-28 12:23:25 +0200

    Fetches new artworks automaticly on a timer, close #5

--- eserver.js
+++ eserver.js
@@ -7,17 +7,20 @@ var request = require('superagent');
 var clientID = '6ea93f8aa8b00ee5d448',
     clientSecret = '04079fc5158cae176d9f598cc81f153e',
     apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
-    xappToken;
+    xappToken,
+    artistLink,
+    intervalExists = false;
 
 var paintingJSON={};
 var artistJSON = {};
 
-var fetchArtist = function(token, call){
+var fetchArtist = function(call){
+  console.log("Artist bein");
   //example request call /artists/andy-warhol
   //https://api.artsy.net/api/artworks?gene_id=4e5e41670d2c670001030350
   request
     .get('https://api.artsy.net/api/'+call)
-    .set('x-xapp-token', token)
+    .set('x-xapp-token', xappToken)
     .set('Accept', 'application/json')
     .end(function(err, res){
       if (err) {
@@ -25,90 +28,102 @@ var fetchArtist = function(token, call){
 
       }else{
 
-        console.log(res.body._embedded.artists[0]);
+        //console.log(res.body._embedded.artists[0]);
         artistJSON =  res.body._embedded.artists[0];
       }
 
+      console.log("Artist End");
+      if (!intervalExists) {
+          intervalExists = true
+          setInterval(fetchPainting, 1000*30);
+      }
 
     });
 }
 
-var fetchPainting = function(token, call){
-  //example request call /artists/andy-warhol
-  //https://api.artsy.net/api/artworks?gene_id=4e5e41670d2c670001030350
+var fetchPainting = function(){
+  console.log("Painting Begin");
   request
-    .get('https://api.artsy.net/api/'+call)
-    .set('x-xapp-token', token)
+    .get('https://api.artsy.net/api/'+ 'artworks?sample=1')
+    .set('x-xapp-token', xappToken)
     .set('Accept', 'application/json')
     .end(function(err, res){
       if (err) {
         console.log(err.error);
 
       }else{
-        fetchArtist(token, res.body._links.artists.href.substring(26));
-
+        //fetchArtist(token, res.body._links.artists.href.substring(26));
+        artistLink = res.body._links.artists.href.substring(26);
         paintingJSON =  res.body;
       }
 
+      console.log("Painting end");
+      fetchArtist(artistLink);
 
     });
 }
 
-request
-  .post(apiUrl)
-  .send({ client_id: clientID, client_secret: clientSecret })
-  .end(function(err, res) {
-    if(err){
-      console.log(err);
-    }else {
+var fetchToken = function(){
+  console.log("Token Begin");
+  request
+    .post(apiUrl)
+    .send({ client_id: clientID, client_secret: clientSecret })
+    .end(function(err, res) {
+      if(err){
+        console.log(err);
+      }else {
 
-       /*
+         /*
 
-       Passing in a parameter of "sample" will redirect you to the
-       canonical URL for a random element in the collection.
-       It can be combo'ed with additional filter parameters, so one
-       could query for a random upcoming show with:
-       "https://api.artsy.net/api/shows?status=upcoming&sample=1"
+         Passing in a parameter of "sample" will redirect you to the
+         canonical URL for a random element in the collection.
+         It can be combo'ed with additional filter parameters, so one
+         could query for a random upcoming show with:
+         "https://api.artsy.net/api/shows?status=upcoming&sample=1"
 
-       */
+         */
 
-       /*
-       Sizes suported by the API:
+         /*
+         Sizes suported by the API:
 
-       //TODO Mke the function automaticly select the largest paintings avalible
+         //TODO Mke the function automaticly select the largest paintings avalible
 
-       featured
-       general
-       large
-       larger
-       large_rectangle
-       medium
-       medium_rectangle
-       normlized
-       square
-       tall
+         featured
+         general
+         large
+         larger
+         large_rectangle
+         medium
+         medium_rectangle
+         normlized
+         square
+         tall
 
-       */
+         */
 
-       fetchPainting(res.body.token, "artworks?sample=1");
+         //fetchPainting(res.body.token, "artworks?sample=1");
 
-       //TODO Fetch artist information for the painting itself also
+          xappToken = res.body.token;
+          //console.log(xappToken);
+          console.log("NEXTINLINE");
+          console.log("Token End");
 
-        xappToken = res.body.token;
-        //console.log(xappToken);
-    }
-});
+          fetchPainting(xappToken);
 
-// set a cookie
+      }
+  });
+
+}
+
+fetchToken();
+
+
 //Executes every time a request is made
-
 app.use(express.static('public'));
 
 app.get('/', function(request, response){
   response.sendFile("index.html");
 });
-
-//TODO Api for client to get the daily painting
 
 //TODO Interupts every hour to get a new painting from the api
 
